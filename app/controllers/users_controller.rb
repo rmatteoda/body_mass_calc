2a6eaa7d28910f0620d63f99ee320733ac708262
Ramiro Matteoda <ramiro.matteoda@3sharecorp.com>
2017-09-11 23:17:11 -0300

    add basic layout and user logged_in check

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
   private
 
   def allowed_params
-    params.require(:user).permit(:email, :password_digest)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
   get 'sessions/new'
 
   resources :sessions
-
   resources :users
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
   root 'welcome#index'
 end
